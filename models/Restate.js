93848e72d1c8f98d124c7bb2ab7f85e51d63c710
pavelgor84 <pavelgor@gmail.com>
2024-01-25 00:08:08 +0400

    mpdels

--- models/Restate.js
+++ models/Restate.js
@@ -1,5 +1,14 @@
-const { Schema } = require("mongoose");
+const { Schema, model, models } = require("mongoose");
 
 const propertySchema = new Schema({
-
+    title: String,
+    bedroom: String,
+    bath: String,
+    ac: String,
+    addons: [{ view: String }, { floor: String }, { parking: String }],
+    price: Number,
+    available: String
 })
+
+const Restate = models?.Restate || model('Restate', propertySchema)
+export default Restate
