bd3707c339da21061eb57280a8635939d022d4af
pavelgor84 <pavelgor@gmail.com>
2024-03-13 23:15:52 +0400

    add Google login

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,32 +1,13 @@
 "use client"
 
 
-
-// import Image from 'next/image'
-
-// import { Swiper, SwiperSlide } from 'swiper/react'
-// import { Pagination, Navigation } from 'swiper/modules'
-
-// import 'swiper/css'
-// import 'swiper/css/navigation'
-// import 'swiper/css/pagination'
-
-// import { images } from './images'
-
-// const images = require.context('../../public/images/south', true)
-// const imageList = images.keys().map(image => images(image));
-
-// imageList.map((image, index) => (
-//     console.log(`   key=${index} src=${JSON.stringify(image.default.src)} alt=${'image' + index} `)
-// ))
-
-
-
+import { auth } from '@/auth'
 import { useEffect, useState } from 'react'
 import styles from './list.module.css'
 
 
 export default function list() {
+
     const [asset, setAsset] = useState([])
     const [property, setProperty] = useState(
         {
--- auth.js
+++ auth.js
@@ -3,7 +3,14 @@ import Google from 'next-auth/providers/google'
 import NextAuth from 'next-auth'
 
 const config = {
-    providers: [Google]
+    providers: [Google],
+    callbacks: {
+        authorized({ request, auth }) {
+            const { pathname } = request.nextUrl;
+            if (pathname === "/admin") return !!auth;
+            return true
+        }
+    }
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
