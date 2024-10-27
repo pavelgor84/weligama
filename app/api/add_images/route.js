9aa84f34b1e0aebcae01a036a62693ce6b519cfd
pavelgor84 <pavelgor@gmail.com>
2024-10-27 18:56:32 +0400

    info update fix

--- app/api/add_images/route.js
+++ app/api/add_images/route.js
@@ -27,19 +27,23 @@ export async function POST(request) {
     }
 
 
-    if (imagesArray.length == 0) {
-        updateInfo(obj_props)
+    if (imagesArray.length == 0) { //If no images included the update info
+        console.log("PROPS UPDATE")
+        const update_info = await Restate.updateOne({ _id: obj_props._id }, { $set: obj_props })
+        //revalidatePath('/', 'layout')
+        return NextResponse.json({ "msg": update_info }, { status: 200 })
+    } else {
+        return new Promise((resolve, reject) => {
+            console.log("IMAGES UPDATE")
+            const uploads = imagesArray.map((im) => UploadImage(im, "sri-lanka"))
+            Promise.all(uploads).then((values) => {
+                updateImages(obj_props, values)
+                revalidatePath('/', 'layout')
+                resolve(NextResponse.json({ "msg": values }, { status: 200 }))
+            }
+            ).catch((err) => reject(err))
+        })
     }
-
-    return new Promise((resolve, reject) => {
-        const uploads = imagesArray.map((im) => UploadImage(im, "sri-lanka"))
-        Promise.all(uploads).then((values) => {
-            updateImages(obj_props, values)
-            revalidatePath('/', 'layout')
-            resolve(NextResponse.json({ "msg": values }, { status: 200 }))
-        }
-        ).catch((err) => reject(err))
-    })
 
     async function updateImages(doc, images) {
 
@@ -48,20 +52,7 @@ export async function POST(request) {
                 { src: images[i].secure_url, width: images[i].width, height: images[i].height, alt: images[i].original_filename, public_id: images[i].public_id }
             )
         }
-        console.log(doc)
+        //console.log(doc)
         await Restate.updateOne({ _id: doc._id }, { $set: doc })
     }
-
-    async function updateInfo(doc) {
-        const update_info = await Restate.updateOne({ _id: doc._id }, { $set: doc })
-        console.log(update_info)
-        //revalidatePath('/', 'layout')
-        return NextResponse.json({ "msg": update_info }, { status: 200 })
-
-    }
-
-
-
-
-
 }
